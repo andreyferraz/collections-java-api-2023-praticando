b2c647cd77596448000858ab4e3dd95e8582ddf1
andreyferraz <andreyferraz@unimedceara.com.br>
2023-08-10 02:26:55 -0300

    operacoes basicas com set

---
+++ src/operacoesbasicascomset/ConjuntoConvidados.java
@@ -0,0 +1,48 @@
+package operacoesbasicascomset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c: convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem "+ conjuntoConvidados.contarConvidados()+" convidado(s) dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Andrêy Martins Ferraz", 1234);
+        conjuntoConvidados.adicionarConvidado("Karine Gonçalves Lima Ferraz", 1235);
+        conjuntoConvidados.adicionarConvidado("Maldoso Lima Ferraz", 1235);
+
+        System.out.println("Existem "+ conjuntoConvidados.contarConvidados()+" convidado(s) dentro do Set de Convidados");
+    }
+
+}
---
+++ src/operacoesbasicascomset/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,47 @@
+package operacoesbasicascomset;
+
+import java.util.Set;
+import java.util.HashSet;
+public class ConjuntoPalavrasUnicas {
+
+    private Set<PalavrasUnicas> palavraSet;
+    
+    public ConjuntoPalavrasUnicas() {
+        this.palavraSet = new HashSet<>();
+    }
+
+    public void adicionarPalavra(String palavra) {
+        palavraSet.add(new PalavrasUnicas(palavra));
+    }
+    
+    public void removerPalavra(String palavra) {
+        PalavrasUnicas palavraUnica = null;
+        for (PalavrasUnicas p : palavraSet) {
+            if (p.getPalavra().equals(palavra)) {
+                palavraUnica = p;
+                break;
+            }
+        } 
+        palavraSet.remove(palavraUnica);           
+    }
+    
+    public boolean verificarPalavra(String palavra) {
+        return palavraSet.contains(new PalavrasUnicas(palavra));
+    }
+
+    public void exibirPalavrasUnicas(){
+        System.out.println(palavraSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas conjuntoPalavrasUnicas = new ConjuntoPalavrasUnicas();
+        conjuntoPalavrasUnicas.adicionarPalavra("André");
+        conjuntoPalavrasUnicas.adicionarPalavra("Mateus");
+        conjuntoPalavrasUnicas.adicionarPalavra("Abobora");
+        conjuntoPalavrasUnicas.removerPalavra("Mateus");
+
+
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+        System.out.println("Existe a palavra abobora? "+ conjuntoPalavrasUnicas.verificarPalavra("André"));
+    }
+}
